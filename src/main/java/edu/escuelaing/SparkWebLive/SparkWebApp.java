37abd7150e8d0844ac98e85ab661d86f12d16e3c
Ernesto Camacho <ernesto.camachoa9805gmail.com>
2021-08-24 15:03:54 -0500

    front listo, before heroku

--- src/main/java/edu/escuelaing/SparkWebLive/SparkWebApp.java
+++ src/main/java/edu/escuelaing/SparkWebLive/SparkWebApp.java
@@ -22,6 +22,28 @@ public class SparkWebApp {
     public static void main(String[] args) {
         staticFiles.location("/public");
         port(getPort());
+
+        options("/*",
+        (request, response) -> {
+
+            String accessControlRequestHeaders = request
+                    .headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers",
+                        accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request
+                    .headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods",
+                        accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
         get("/results", (req, res) -> resultsPage(req, res));
         get("/facade", (req,res) -> facadePage(req, res));
     }
